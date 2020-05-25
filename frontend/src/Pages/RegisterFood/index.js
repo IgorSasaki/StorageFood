56366492e1b50c1f4d42cd721b5b007f12adc2df
Igor Sasaki <igor-sasaki@hotmail.com>
2020-05-25 18:04:00 -0300

    Implementação do metodo de cadastro de alimentos utilizando axios

--- frontend/src/Pages/RegisterFood/index.js
+++ frontend/src/Pages/RegisterFood/index.js
@@ -1,12 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import api from '../../Services/API';
 
 import './style.css';
 
 import logoImg from '../../Assets/EB_Logo.png';
 
 export default function RegisterFood() {
+    const [descricao, setDescricao] = useState('');
+    const [medida, setMedida] = useState('');
+    const [unidade, setUnidade] = useState('');
+    const [quantidade, setQuantidade] = useState(1);
+
+    async function handleRegisterFood(event){
+        event.preventDefault();
+
+        const dados = {
+            descricao, medida, unidade, quantidade
+        }
+
+        try {
+            await api.post('createFood', dados);
+
+            alert('Registrado com sucesso')
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <div className="registerFood-container">
             <div className="content">
@@ -21,9 +44,10 @@ export default function RegisterFood() {
                         </Link>
                 </section>
 
-                <form>
-                    <input type="text" placeholder="Descrição" />
-                    <select>
+                <form onSubmit={handleRegisterFood}>
+                    <input type="text" placeholder="Descrição" value={descricao}
+                        onChange={(event) => setDescricao(event.target.value)} />
+                    <select onChange={(event) => setUnidade(event.target.value)}>
                         <optgroup label="Unidade de Medida">
                             <option defaultValue hidden> Unidade de Medida </option>
 
@@ -31,8 +55,10 @@ export default function RegisterFood() {
                             <option value="l">Litro</option>
                         </optgroup>
                     </select>
-                    <input type="text" placeholder="Medida" />
-                    <input type="number" placeholder="Quantidade" />
+                    <input type="text" placeholder="Medida" value={medida}
+                        onChange={(event) => setMedida(event.target.value)} />
+                    <input type="number" placeholder="Quantidade" value={quantidade}
+                        onChange={(event) => setQuantidade(event.target.value)} />
 
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
